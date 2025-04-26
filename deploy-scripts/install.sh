45a847e61eb6f24a2e8f9de23f9d884a2cbbcbf5
Evil404Dead <evil404dead@gmail.com>
2025-04-26 14:59:06 +0530

    new

--- deploy-scripts/install.sh
+++ deploy-scripts/install.sh
@@ -2,7 +2,7 @@
  echo "Deployment started..."
  
  # Optional: make sure permissions are good
- sudo chmod -R 755 /var/www/html
+ chmod -R 755 /var/www/html
  
  # Optional: restart Nginx if needed
  systemctl restart nginx
